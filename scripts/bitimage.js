2816fa9f35a51adbb1eec35f77fbbafcf019a04a
Paul <phunter510@gmail.com>
2018-11-04 01:09:26 -0500

    binary added

--- scripts/bitimage.js
+++ scripts/bitimage.js
@@ -3,6 +3,7 @@ const row = 8
 const col = 5
 
 let bitimg = document.getElementById('bitimage');
+let binary = document.getElementById('binary');
 
 let tile = new Array(row);
 
@@ -39,4 +40,42 @@ function click(event) {
 	source.src = 'images/white.svg';
 	source.onoff = 0;
     }
+    binary.innerHTML = toBinaryString(tile[0]) + " " + toHexString(tile[0]);
+}
+
+function toBinaryString(tileArr) {
+    let bin_str = '000';
+    for (let i = 0; i < tileArr.length; i++) {
+	bin_str = bin_str + String(tileArr[i].onoff);
+    }
+    return bin_str;
+}
+
+function toHexString(tileArr) {
+    let hex_str = '0x';
+    let hex_num = 0;
+//    let hex_let = '';
+    
+    hex_str = hex_str + tileArr[0].onoff;
+    
+    for (let i = 1; i < tileArr.length; i++) {
+	hex_num += tileArr[i].onoff * 2 ** (4-i)
+    }
+
+    switch(hex_num) {
+    case 10:
+	hex_num = 'A';
+    case 11:
+	hex_num = 'B';
+    case 12:
+	hex_num = 'C';
+    case 13:
+	hex_num = 'D';
+    case 14:
+	hex_num = 'E';
+    case 15:
+	hex_num = 'F';
+    }
+    
+    return hex_str + hex_num;
 }
